876869b3721e7811251a408a582f4d34289fde1a
Spencer Nguyen <shn1@students.uwf.edu>
2021-04-22 21:02:14 -0500

    final save

--- LanguageDecoder/src/Tree.java
+++ LanguageDecoder/src/Tree.java
@@ -1,12 +1,20 @@
+/*********************************************************************** 
+  Student Name: Spencer Nguyen
+  File Name: LanguageDecoder
+  Assignment Number: Project 3
+  Course: COP5416
 
+This class is the tree object for use with Strings only. It accepts a 
+key and inserts the key and words into a tree. It will also traverse
+the tree and print the corresponding word.
+************************************************************************/
 public class Tree{
 
 	private Node root;
 	
 	Tree(){
-		this.root = new Node("");
+		this.root = new Node("");// initialize root as empty 
 	}
-	
 	
 	private class Node{
 		
@@ -24,16 +32,35 @@ public class Tree{
 		}
 	}
 	
+	/**
+	 * This method accepts data and the designator "key". It
+	 * begins the iterative insert with root and passed data.
+	 * @param data
+	 * @param designator
+	 */
 	public void insert(String data, String designator) {
 		insertIterative(this.root, data, designator);
 	}
 	
-	public void insertIterative(Node current, String data, String designator) {
-		
+	/**
+	 * This method accepts the encoded message and will traverse the
+	 * tree as an iterative search. It begins the search at root.
+	 * @param encoded
+	 */
+	public void search(String encoded) {
+		searchIterative(this.root, encoded);
+	}
+	
+	/**
+	 * This method inserts iteratively. It accepts the node, data, and designator "key".
+	 * In this method, '+' inserts left and '-' inserts right.
+	 * @param current
+	 * @param data
+	 * @param designator
+	 */
+	private void insertIterative(Node current, String data, String designator) {
 		
 		for(int i = 0; i < designator.length(); i++) {
-			
-
 			if(designator.charAt(i) == '+') {
 				if(current.left == null) {
 					current.left = new Node(data);
@@ -53,27 +80,28 @@ public class Tree{
 		}
 	}
 	
-	public void search(String encoded) {
-		searchIterative(this.root, encoded);
-	}
-	
-	public void searchIterative(Node current , String encoded) {
+	/**
+	 * This method traverses the tree iteratively. It accepts the encoded
+	 * message and will traverse the tree based on the symbols in the message.
+	 * @param current
+	 * @param encoded
+	 */
+	private void searchIterative(Node current , String encoded) {
 		
 		for(int i = 0; i < encoded.length(); i++) {
-			
-
 			if(encoded.charAt(i) == '+') {
 				current = current.left;
+				/* If last node is reached. */
 				if(i == encoded.length() - 1) {
 					System.out.print(current.getData() + " ");
 				}
 			}
 			else if(encoded.charAt(i) == '-') {
 				current = current.right;
+				/* If last node is reached. */
 				if(i == encoded.length() - 1) {
 					System.out.print(current.getData() + " ");
 				}
-
 			}
 		}
 	}
--- LanguageDecoder/src/main.java
+++ LanguageDecoder/src/main.java
@@ -1,3 +1,13 @@
+/*********************************************************************** 
+  Student Name: Spencer Nguyen
+  File Name: LanguageDecoder
+  Assignment Number: Project 3
+  Course: COP5416
+
+This file accepts a "key" text file for a language decoder. It will
+automatically build a tree with the key. It will also accept encoded 
+messages and automatically decode and print the message.
+************************************************************************/
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -14,7 +24,6 @@ public class main {
 		String encodedLine  = "";
 		String encodedWord  = "";
 		String[] wordAndKey = new String[2];
-
 		
 		File wordFile    = new File(wordsKeyTxt);
 		File encodedFile = new File(encodedTxt);
@@ -22,32 +31,28 @@ public class main {
 		Scanner scnrWord    = new Scanner(wordFile);
 		Scanner scnrEncoded = new Scanner(encodedFile);
 
+		/* Build tree based on the decoder key. */
 		while(scnrWord.hasNextLine()) {
-			
 			wordLine   = scnrWord.nextLine();
 			wordAndKey = wordLine.split(" ", 2);
-			decoder.insert(wordAndKey[0], wordAndKey[1]);
+			decoder.insert(wordAndKey[0], wordAndKey[1]);// the first component is the word and the second component is the symbol
 		}
-		
 		scnrWord.close();
 		
+		/* Traverse tree and print decoded text. */
 		while(scnrEncoded.hasNextLine()) {
-			
 			encodedLine = scnrEncoded.nextLine();
 			
 			Scanner parseEncoded = new Scanner(encodedLine);
 			
+			/* Get each word from a line. */
 			while(parseEncoded.hasNext()) {
-				
 				encodedWord = parseEncoded.next();
 				decoder.search(encodedWord);
 			}
-			
 			parseEncoded.close();
-			
 			System.out.println();		
 		}
-		
 		scnrEncoded.close();	
 	}
 }
